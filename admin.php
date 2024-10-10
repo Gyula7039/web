8105fc1928f005ac316c40562bd9c7563d4db3f7
Gyula7039 <168169381+Gyula7039@users.noreply.github.com>
2024-10-10 10:55:43 +0200

    NO DELETE

--- admin.php
+++ admin.php
@@ -28,6 +28,7 @@ if (isset($_POST['delete_user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
     <title>Admin felület</title>
 </head>
 <body>
